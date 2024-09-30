4ae024af8c673897a7801761c9eb126e6d287780
AyaM9999 <162885810+AyaM9999@users.noreply.github.com>
2024-09-30 04:38:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let i = document.querySelector(".links");
+let x = document.getElementsByClassName("links");
+
+function myFunction(){
+     let i = document.querySelector(".links");
+      if (i.style.display === "none"){i.style.display = "block"; }
+      else{ i.style.display = "none";}
+     
+}
+
+function FunctionOut(){
+     if (i.style.display = "block"){i.style.display = "none"; }
+}
